744f9916fa54a22608850df6799d371d3d24a005
Drakaris8866 <109422904+Drakaris8866@users.noreply.github.com>
2022-12-06 12:55:04 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,21 @@ import { AppModule } from './app.module'
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	app.setGlobalPrefix('api')
+	app.use((req, res, next) => {
+		res.header('Access-Control-Allow-Origin', '*');
+		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+		res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+		next();
+	});
 	app.enableCors({
-            origin: true,
-            methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-            credentials: true,
-        });
+		origin: [
+			'http://localhost:3000',
+		],
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		credentials: true,
+	});
 	await app.listen(4200)
 }
 bootstrap()
+
 
