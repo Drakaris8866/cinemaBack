4fc1b4fecbefd039cbd5837da55eb0fc079b0d2e
Drakaris8866 <109422904+Drakaris8866@users.noreply.github.com>
2022-12-07 22:36:04 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,22 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import * as express from 'express';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	app.setGlobalPrefix('api')
 	app.use((req, res, next) => {
-  	res.header('Access-Control-Allow-Origin', '*');
- 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  	res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-  	next();
+		res.header('Access-Control-Allow-Origin', '*');
+		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+		res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+		next();
 	});
-	app.enableCors();
-	app.use(express.static(join(__dirname, '..' ,'uploads')))
+	app.enableCors({
+		origin: [
+			'http://localhost:3000',
+		],
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		credentials: true,
+	});
 	await app.listen(4200)
 }
 bootstrap()
-
-
