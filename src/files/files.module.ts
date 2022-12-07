b4b8a683e1091834ab55677f468d3661345004e9
Drakaris8866 <109422904+Drakaris8866@users.noreply.github.com>
2022-12-07 22:27:09 +0300

    Update files.module.ts

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,15 +1,14 @@
 import { Module } from '@nestjs/common'
 import { ServeStaticModule } from '@nestjs/serve-static'
-import { path } from 'app-root-path'
+import { join } from 'path';
 
 import { FilesController } from './files.controller'
 import { FilesService } from './files.service'
-console.log(path)
+
 @Module({
 	imports: [
 		ServeStaticModule.forRoot({
-			rootPath: `${path}/uploads`,
-			serveRoot: '/uploads',
+			rootPath: join(__dirname, '..', '..', 'uploads'),
 		}),
 	],
 	providers: [FilesService],
